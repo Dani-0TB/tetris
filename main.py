bcb0e5237a9e6fd443e64285806aaf08b8756bde
Dani Ocaranza Navarrete <dani_0tb@pop-os.localdomain>
2022-06-21 13:18:35 -0400

    Edited Tetramino Class, now much simpler

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from tabnanny import check
 import pygame
 from settings import *
 from sys import exit
-
 from tetramino import *
 
 def main():
@@ -15,7 +13,7 @@ def main():
     clock = pygame.time.Clock()
 
 
-    current_piece = O(3,-4)
+    current_piece = Tetramino(3,-4)
 
     while True:
         for event in pygame.event.get():
@@ -23,24 +21,20 @@ def main():
                 pygame.quit()
                 exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and not check_collision(current_piece,"L"):
+                if event.key == pygame.K_LEFT:
                     current_piece.x-=1
-                if event.key == pygame.K_RIGHT and not check_collision(current_piece,"R"):
+                if event.key == pygame.K_RIGHT:
                     current_piece.x+=1
-                if event.key == pygame.K_DOWN and not check_collision(current_piece,"D"):
+                if event.key == pygame.K_DOWN:
                     current_piece.y+=1
                 if event.key == pygame.K_r:
-                    current_piece = O(0,0)
+                    current_piece = Tetramino(3,0)
 
-        current_piece.update()
-        if check_collision(current_piece,"D"):
-            for piece_y in range(4):
-                for piece_x in range(4):
-                    if current_piece.shapes[0][piece_y][piece_x]:
-                        game_stack[current_piece.y+piece_y][current_piece.x+piece_x]=True
-                        color_stack[current_piece.y+piece_y][current_piece.x+piece_x]=current_piece.color
-            current_piece = O(3,-4)
-            current_piece.color+=1
+        if current_piece.move_timer < 60:
+            current_piece.move_timer+=1
+        else:
+            current_piece.move_timer=0
+            current_piece.y+=1
 
         draw_field(screen, current_piece)
 
@@ -62,30 +56,7 @@ def draw_field(screen, current_piece):
             screen.blit(cell_surf,cell_rect)
 
     # Draw current piece
-    draw_piece(current_piece,screen)
-
-
-def draw_piece(piece,screen):
-    for piece_y in range(4):
-        for piece_x in range(4):
-            if piece.shapes[0][piece_y][piece_x]:
-                piece_surf = pygame.surface.Surface((tile_size-border,tile_size-border))
-                piece_rect = piece_surf.get_rect(topleft = ((piece.x+piece_x)*tile_size, (piece.y+piece_y)*tile_size))
-                piece_surf.fill(colors[piece.color])
-                screen.blit(piece_surf,piece_rect)
-
-    
-def check_collision(piece,dir):
-    # LEFT
-    for piece_y in range(4):
-        for piece_x in range(4):
-            if piece.shapes[0][piece_y][piece_x]:
-                if piece.x+piece_x-1 == -1 and dir == "L":
-                    return True
-                if piece.x+piece_x+1 == 10 and dir == "R":
-                    return True
-                if piece.y+piece_y+1==20 and dir == "D":
-                    return True
+    current_piece.draw(screen)
 
 if __name__ == "__main__":
     main()
--- tetramino.py
+++ tetramino.py
@@ -1,36 +1,43 @@
-colors = [(0,0,0),
-    (235, 64, 52),
-    (235, 201, 52),
-    (24, 181, 24),
-    (113, 206, 235), 
-    (223, 113, 235),
-    (237, 137, 43)]
+import pygame
+from random import randint
+from settings import *
+
+colors = [(0,0,0),  #BLACK
+    (232, 183, 21), #YELLOW
+    (245, 71, 32),  #ORANGE
+    (11, 3, 161),   #BLUE
+    (42, 179, 27),  #GREEN
+    (227, 16, 16),  #RED
+    (57, 14, 138),  #PURPLE
+    (99, 182, 230)] #SKY
+
+shapes = [
+    [[1,2,5,6]],    #O 0
+    [[1,5,9,10]],   #L 1
+    [[1,5,8,9]],    #J 2
+    [[1,5,6,10]],   #N 3
+    [[0,1,5,6]],    #Z 4
+    [[1,4,5,6]],    #T 5
+    [[2,6,10,14]]   #I 6
+]
 
 class Tetramino:
-    def __init__(self):
+    def __init__(self, x, y):
         self.move_timer = 0
-        self.shape = [
-            [False,False,False,False],
-            [False,False,False,False],
-            [False,False,False,False],
-            [False,False,False,False]]
-
-    def update(self):
-        if self.move_timer < 60:
-            self.move_timer += 1
-        else:
-            self.move_timer=0
-            self.y+=1
-
-
-class O(Tetramino):
-    def __init__(self,x,y):
-        super().__init__()
         self.x=x
         self.y=y
-        self.color = 2
-        self.shapes = [[
-            [False,False,False,False],
-            [False,True,True,False],
-            [False,True,True,False],
-            [False,False,False,False]]]
+        self.shape=randint(0,len(shapes)-1)
+        self.rotation=0
+        self.shapes=shapes[self.shape][self.rotation]
+        self.color=self.shape+1
+    
+    def draw(self,screen):
+        counter = 0
+        for cell_y in range(4):
+            for cell_x in range(4):
+                if counter in self.shapes:
+                    piece_surf = pygame.surface.Surface((tile_size-border,tile_size-border))
+                    piece_rect = piece_surf.get_rect(topleft = ((self.x+cell_x)*tile_size, (self.y+cell_y)*tile_size))
+                    piece_surf.fill(colors[self.color])
+                    screen.blit(piece_surf,piece_rect)
+                counter += 1
